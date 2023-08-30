f9ec10a91b5ced3d36d8557367641a38db734f3a
Nguyen-Phu-B <nguyenhphub@gmail.com>
2023-08-30 20:49:21 +0700

    ...

--- src/components/Layouts/Header/Header.jsx
+++ src/components/Layouts/Header/Header.jsx
@@ -7,42 +7,6 @@ const cx = classNames.bind(styles);
 
 const Header = () => {
   return (
-    // <>
-    //   <nav className="bg-gray-900 py-4">
-    //     <div className="container mx-auto px-4">
-    //       <div className="flex items-center justify-between">
-    //         <div className="text-white font-bold text-xl">My Website</div>
-    //         <ul className="flex space-x-4">
-    //           <li>
-    //             <Link
-    //               to="/"
-    //               className="text-white hover:text-gray-300 transition duration-300"
-    //             >
-    //               Home
-    //             </Link>
-    //           </li>
-    //           <li>
-    //             <Link
-    //               to="/login"
-    //               className="text-white hover:text-gray-300 transition duration-300"
-    //             >
-    //               Login
-    //             </Link>
-    //           </li>
-    //           <li>
-    //             <Link
-    //               to="/register"
-    //               className="text-white hover:text-gray-300 transition duration-300"
-    //             >
-    //               Register
-    //             </Link>
-    //           </li>
-    //         </ul>
-    //       </div>
-    //     </div>
-    //   </nav>
-    // </>
-
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
         <Link className={cx("logo")} to="/">
@@ -53,14 +17,23 @@ const Header = () => {
           <button className={cx("search-btn", "text-gray-400 font-sm")}>
             <i className="fa-solid fa-magnifying-glass"></i>
           </button>
-          <input type="text" placeholder="Start typing to search.." />
+          <input
+            className="font-xsss"
+            type="text"
+            placeholder="Start typing to search.."
+          />
         </div>
 
         <div className={cx("actions")}>
-          <Link to="/">1</Link>
-          <Link to="/">2</Link>
-          <Link to="/">3</Link>
-          <Link to="/">4</Link>
+          <button className={cx("icon")}>
+            <i class="fa-regular fa-bell"></i>
+          </button>
+          <button className={cx("icon")}>
+            <i class="fa-regular fa-message"></i>
+          </button>
+          <Link to="/login" className={cx("logo-user")}>
+            <img src="http://uitheme.net/sociala/images/profile-4.png" alt="" />
+          </Link>
         </div>
       </div>
     </header>
